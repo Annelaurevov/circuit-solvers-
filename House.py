d68e8ec6e264735537f61ce194d211718bf8755b
Bloemen <j.bloemen@ci-engineers.com>
2024-01-09 12:55:06 +0100

    small adjusts

--- House.py
+++ House.py
@@ -1,12 +1,21 @@
 # House Class
 
 class House:
-    def __init__(self, position, capacity):
+    """
+    Usage:
+
+    init(position, capacity, id: int)
+
+    make_path(position)
+    Makes path to certain position
+    """
+    def __init__(self, position: tuple[int, int], capacity: float, id: int):
         self.position = position
         self.capacity = capacity
         self.path = []
+        self.id = id
     
-    def make_path(self, position):
+    def make_path(self, position: tuple[int, int]) -> None:
         "Make path to other position"
         self.path = [self.position]
         posx, posy = position
