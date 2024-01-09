2d3bfa8b49a05203cdf4ffc7f34ba63bff877ac8
annelaure1111 <74504026+annelaure1111@users.noreply.github.com>
2024-01-09 13:38:35 +0100

    Update battery.py

--- battery.py
+++ battery.py
@@ -1,4 +1,10 @@
 class battery():
+    """
+    Usage:
+
+    add(self, house)
+    remove(self, house)
+    """
 
     def __init__(self, full_capacity, capacity, id: int) -> None:
         self.full = full_capacity
@@ -6,18 +12,19 @@ class battery():
         self.list = []
         self.id = id
 
-    def add(self, house: int) -> None:
+    def add(self, house_id: int) -> bool:
         "Adds the house, after checking if there is enough capacity"
         # adds houses to the battery
         if self.capacity >= house.capacity:
             self.capacity -= house.capacity
-            self.list.append(house)
+            self.list.append(house_id)
             return True
         else:
             return False
 
-    def remove(self) -> None:
+    def remove(self, house_id: int) -> None:
         # removes house from stack
-        for i, house in enumerate(self.list):
+        for i, house_id in enumerate(self.list):
             list.pop(i)
             return
+            
