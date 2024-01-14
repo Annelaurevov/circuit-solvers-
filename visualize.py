abf42a1d56941e485d52e75f238878fad57d883c
Salomé Poulain <salome.poulain@student.uva.nl>
2024-01-14 14:07:00 +0100

    visualise opgedeelt in functies

--- visualize.py
+++ visualize.py
@@ -1,113 +1,93 @@
-# tool om te visualiseren welke connecties er zijn
-
-
 import pygame
 import json
 
 file = open(r"outputs/output_district-2.json", 'r')
-# file = open(r"test.json", 'r')
-
-
-
 data = json.load(file)
  
-# Define some colors
 BLACK = (0, 0, 0)
-
 WHITE = (255, 255, 255)
-GREEN = (0, 210, 0)
-RED = (255, 0, 0)
 GRAY = (220, 220, 220)
 BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 210, 0)
 PURPLE = (255, 0, 255)
 ORANGE = (255, 150, 0)
 colors = [BLUE, RED, GREEN, PURPLE, ORANGE]
 
 pygame.init()
 
-
-# Set the width and height of the screen [width, height]
 size = (800, 800)
 gridsize = (60, 60)
 screen = pygame.display.set_mode(size)
-
-battery = pygame.image.load(r'images/battery.png')
-battery = pygame.transform.scale(battery, (2*size[0]//gridsize[0], 2*size[1]//gridsize[1]))
-
-house = pygame.image.load(r'images/house2.png').convert_alpha()
-house = pygame.transform.scale(house, (1*size[0]//gridsize[0], 1*size[1]//gridsize[1]))
-
+box_width = size[0] // gridsize[0]
+box_height = size[1] // gridsize[1]
 
 pygame.display.set_caption("My Game")
- 
-# Loop until the user clicks the close button.
 done = False
- 
-# Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+
+battery = pygame.image.load(r'images/battery.png')
+battery = pygame.transform.scale(battery, (2 * box_width, 2 * box_height))
+
+house = pygame.image.load(r'images/house.png').convert_alpha()
+house = pygame.transform.scale(house, (1 * box_width, 1 * box_height))
 
 
 def get_on_screen_coordinates(x, y):
-    return [y * size[0]//gridsize[0], x * size[1]//gridsize[1]]
-
-
-def create_colored_battery(color):
-    colored_battery = pygame.Surface(battery.get_size(), pygame.SRCALPHA)
-    colored_battery.fill(color)
-    colored_battery.blit(battery, (0, 0))
-    return colored_battery
+    return [y * box_width, x * box_height]
 
 
 def draw_grid(size, gridsize):
     for i in range(gridsize[0]):
         for j in range(gridsize[1]):
-            pygame.draw.line(screen, GRAY, [0, j * size[1] // gridsize[1]], [size[0], j * size[1] // gridsize[1]])
-            pygame.draw.line(screen, GRAY, [i * size[0] // gridsize[0], 0], [i * size[0] // gridsize[0], size[1]])
+            pygame.draw.line(screen, GRAY, [0, j * box_height], [size[0], j * box_height])
+            pygame.draw.line(screen, GRAY, [i * box_width, 0], [i * box_width, size[1]])
 
 
-def draw_on_screen(screen):
+def draw_battery(screen, color, location):
+    battery_location = [location[0] - 1, location[1] - 2]
 
+    battery_underlay_rect = pygame.Rect(get_on_screen_coordinates(*battery_location), (2 * box_width, 2 * box_height))
+    pygame.draw.rect(screen, color, battery_underlay_rect)
+
+    battery.set_alpha(130)
+    screen.blit(battery, get_on_screen_coordinates(*battery_location))
+
+
+def draw_house(screen, color, location):
+    house_location = [location[0] - 0.5, location[1] - 0.5]
+
+    house_underlay_rect = pygame.Rect(get_on_screen_coordinates(*house_location), (box_width, box_height))
+    pygame.draw.rect(screen, color, house_underlay_rect)
+
+    house.set_alpha(130)
+    screen.blit(house, get_on_screen_coordinates(*house_location))
+
+
+def draw_cables(screen, color, cables):
+    starting_point = tuple(map(int, cables[0].split(",")))
+    for end_point in cables[1:]:
+        end_point = tuple(map(int, end_point.split(",")))
+
+        pygame.draw.line(screen, color, get_on_screen_coordinates(*starting_point),
+                         get_on_screen_coordinates(*end_point), width=2)
+
+        starting_point = end_point
+
+
+def draw_all(screen):
     draw_grid(size, gridsize)
     
     id = 0
     for location_data in data[1:]:
         color = colors[id]
         id += 1
-        battery_location = list(map(int, location_data['location'].split(',')))
-        battery_location = [battery_location[0] - 1, battery_location[1] - 2]
+        
+        draw_battery(screen, color, tuple(map(int, location_data['location'].split(','))))
 
-        # Draw battery on top
-        screen.blit(create_colored_battery(color), get_on_screen_coordinates(*battery_location))
-
-        for houses in location_data["houses"]:
-            house_location = list(map(int, houses['location'].split(',')))
-            house_location = [house_location[0] -0.5, house_location[1] - 0.5]
-
-            # Draw house underlay with battery color
-            house_underlay_rect = pygame.Rect(get_on_screen_coordinates(*house_location), (size[0] // gridsize[0], size[1] // gridsize[1]))
-            pygame.draw.rect(screen, color, house_underlay_rect)
-
-            # Draw house image on top with adjusted opacity
-            house.set_alpha(150) 
-            screen.blit(house, get_on_screen_coordinates(*house_location))
-
-            cables = houses["cables"]
-
-            starting_point = tuple(map(int, cables[0].split(",")))
-            for end_point in cables[1:]:
-                end_point = tuple(map(int, end_point.split(",")))
-
-                pygame.draw.line(screen, color, get_on_screen_coordinates(*starting_point),
-                                 get_on_screen_coordinates(*end_point), width=2)
-
-                starting_point = end_point
-
-
-
-
-
-
-
+        for house_data in location_data["houses"]:
+            draw_house(screen, color, tuple(map(int, house_data['location'].split(','))))
+            draw_cables(screen, color, house_data["cables"])
 
 
 # -------- Main Program Loop -----------
@@ -116,26 +96,26 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+
     # --- Game logic should go here
- 
+
     # --- Screen-clearing code goes here
- 
+
     # Here, we clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
- 
+
     # If you want a background image, replace this clear with blit'ing the
     # background image.
     screen.fill(WHITE)
- 
+
     # --- Drawing code should go here
-    draw_on_screen(screen)
- 
+    draw_all(screen)
+
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
- 
+
     # --- Limit to 60 frames per second
     clock.tick(60)
- 
+
 # Close the window and quit.
 pygame.quit()
