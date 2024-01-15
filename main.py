d21dfb36ec769b61b91520f4809bb17de26011eb
Bloemen <j.bloemen@ci-engineers.com>
2024-01-15 14:09:52 +0100

    Cleaned up folder

--- main.py
+++ main.py
@@ -1,53 +1,50 @@
-from Grid import Grid
-from House import House
-from battery import Battery
+import sys
+
+from code.classes.Grid import Grid
+from code.classes.House import House
+from code.classes.Battery import Battery
+
+from code.algoritmen.fill_grid import fill_grid_greedy
+from code.algoritmen.switch_pairs import switch_pairs
+
+from code.vizualization.visualize import visualize
 
 
-def distance(object1, object2):
-    "Returns the distance between two objects"
-    x1, y1 = object1.position
-    x2, y2 = object2.position
-    return abs(x1-x2) + abs(y1 - y2)
+if len(sys.argv) == 1:
+    print("Usage: python main.py [-v] <district_number>")
+    sys.exit(1)
 
+arguments = sys.argv[1:]
+
+needs_visualize = False
+district = None
+
+for arg in arguments:
+    if arg == "-v":
+        needs_visualize = True
+
+    if "1" <= arg <= "3":
+        district = int(arg)
+
+if district is None:
+    print("Usage: python main.py [-v] <district_number>")
+    sys.exit(1)
 
 if __name__ == "__main__":
-    grid = Grid(1)
-    grid.load_houses(r"Huizen&Batterijen/district_1/district-1_houses.csv")
-    grid.load_batteries(r"Huizen&Batterijen/district_1/district-1_batteries.csv")
-    to_be_placed = grid.houses.copy()
+    grid = Grid(district)
+    grid.load_houses(r"data/district_X/district-X_houses.csv".replace("X", str(district)))
+    grid.load_batteries(r"data/district_X/district-X_batteries.csv".replace("X", str(district)))
+    fill_grid_greedy(grid)
 
-    while to_be_placed:
-        house_list = []
+    assert grid.is_filled()
+    print("Cost = ", grid.calc_costs())
 
-        while to_be_placed:
-            house = to_be_placed.pop()
+    while switch_pairs(grid):
+        print("New cost: ", grid.calc_costs())
 
-            best_distance = 1e100
-            best_battery = None
+    assert grid.is_filled()
 
-            for battery in grid.batteries:
-                if not battery.can_add(house):
-                    continue
-                if distance(house, battery) < best_distance:
-                    best_distance = distance(house, battery)
-                    best_battery = battery
-            if best_battery is None:
+    grid.write_out(r"data/outputs/output_district-X.json".replace("X", str(district)))
 
-                grid.write_out("test.json")
-
-            house_list.append((best_distance, best_battery, house))
-            #print(min((distance(house,battery), battery, house) for battery in grid.batteries))
-
-        house_list.sort(reverse=True, key=lambda x: x[0])
-        while house_list:
-
-            _, battery, house = house_list.pop()
-            if battery is None:
-                break
-            if not battery.can_add(house):
-                to_be_placed.append(house)
-                print("oeps")
-            else:
-                battery.add(house)
-
-    grid.write_out("test.json")
+    if needs_visualize:
+        visualize(district)
