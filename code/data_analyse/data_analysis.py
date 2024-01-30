43c7072b047c3c0dfd40e76c09cb1967fdc2bde8
Annelaure van Overbeeke <74504026+Annelaurevov@users.noreply.github.com>
2024-01-30 13:32:05 +0100

    doc strings and type hints

--- code/data_analyse/data_analysis.py
+++ code/data_analyse/data_analysis.py
@@ -1,10 +1,22 @@
 # File containing functions used for data analysis
+from typing import List, Union
 
-def get_average(data: list[int | float]) -> float:
+def get_average(data: List[Union[int, float]]) -> float:
+    """
+    Calculate the average of a list of numerical data.
+
+    Args:
+        data (List[Union[int, float]]): A list of numerical data.
+    """
     return sum(data) / len(data)
 
+def get_deviation(data: List[Union[int, float]]) -> float:
+    """
+    Calculate the standard deviation of a list of numerical data.
 
-def get_deviation(data: list[int | float]):
+    Args:
+        data (List[Union[int, float]]): A list of numerical data.
+    """
     S2 = 0
 
     avg = get_average(data)
@@ -12,12 +24,23 @@ def get_deviation(data: list[int | float]):
 
     for point in data:
         S2 += (point - avg)**2
-    
+
     return (S2 / (n - 1))**0.5
 
+def get_low(data: List[Union[int, float]]) -> Union[int, float]:
+    """
+    Get the lowest value from a list of numerical data.
 
-def get_low(data):
+    Args:
+        data (List[Union[int, float]]): A list of numerical data.
+    """
     return min(data)
 
-def get_high(data):
+def get_high(data: List[Union[int, float]]) -> Union[int, float]:
+    """
+    Get the highest value from a list of numerical data.
+
+    Args:
+        data (List[Union[int, float]]): A list of numerical data.
+    """
     return max(data)
